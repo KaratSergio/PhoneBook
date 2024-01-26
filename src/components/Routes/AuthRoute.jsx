12df7c92c087d4351a17ef44fa3fa793770a0bfd
Sergio <137483700+KaratSergio@users.noreply.github.com>
2024-01-26 09:14:52 +0200

    auth slice

---
+++ src/components/Routes/AuthRoute.jsx
@@ -0,0 +1,16 @@
+import { Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import {
+  selectIsLoggedIn,
+  selectIsRefreshing,
+} from '../../redux/auth/selectors';
+
+const AuthRoute = ({ component: Component, redirectTo = '/' }) => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const isRefreshing = useSelector(selectIsRefreshing);
+
+  const shouldRedirect = !isLoggedIn && !isRefreshing;
+  return shouldRedirect ? <Navigate to={redirectTo} /> : <Component />;
+};
+
+export default AuthRoute;
