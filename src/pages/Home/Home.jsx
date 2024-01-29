3d256b5fcf409497afc25ab385e8f7244ccae89c
Sergio <137483700+KaratSergio@users.noreply.github.com>
2024-01-29 14:11:01 +0200

    add react logo + favicon

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import { Logo } from './Logo/Logo';
 import css from './Home.module.css';
+import iconReact from '../../img/react-logo.png';
 
 export const Home = () => {
   return (
@@ -8,7 +9,10 @@ export const Home = () => {
         <h1>
           Welcome to <span>Phonebook</span>!
         </h1>
-        <p>created on react</p>
+        <div className={css['react-logo-box']}>
+          <p>created on <span>React</span></p>
+          <img className={css['react-logo']} src={iconReact} alt="Icon" />
+        </div>
       </div>
       <div>
         <Logo />
